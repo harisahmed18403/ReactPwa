33fec4537c9351e8f1643396b9f75c7592c53dbe
harisahmed18403 <harisahmed18403@gmail.com>
2024-12-15 22:12:51 +0000

    componets and router

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../components/Hero'
+
+const HomePage = () => {
+    return (
+        <>
+            <Hero title="My Title" subtitle="my subtitle" />
+        </>
+    )
+}
+
+export default HomePage
